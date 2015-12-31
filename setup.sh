dbb30891585c0cdf80cb3be098132bd3dc7a7979
Darrell Mozingo <darrell@mozingo.net>
2015-12-31 13:56:55 +0000

    Add tmux config with mouse support

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -sv ~/.dotfiles/.vimrc ~
 ln -sv ~/.dotfiles/.zshrc ~
+ln -sv ~/.dotfiles/.tmux.conf ~
 vim +PluginInstall
