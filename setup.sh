4c484be8c9fb4e24efc54a7ff01ea2827446ed18
Darrell Mozingo <darrell@mozingo.net>
2016-08-12 14:54:56 +0100

    Install oh-my-zshell last
    It kicks you into the new shell so vim never setup. Also install git & curl just to be safe.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 sudo apt-get update \
-  && sudo apt-get install -y vim tmux zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  && sudo apt-get install -y git curl vim tmux zsh
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -sv ~/.dotfiles/.vimrc ~
 ln -sv ~/.dotfiles/.zshrc ~
 ln -sv ~/.dotfiles/.tmux.conf ~
 vim +PluginInstall
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
