665afde4bdae24155a32864710c02e0a13787724
Darrell Mozingo <darrell@mozingo.net>
2016-08-12 14:25:07 +0100

    Install vim, tmux, and zsh as part of setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo apt-get update \
+  && sudo apt-get install -y vim tmux
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -sv ~/.dotfiles/.vimrc ~
 ln -sv ~/.dotfiles/.zshrc ~
