4e97efd651bea4c5bb4e76563b0a241c1f989692
Darrell Mozingo <darrell@mozingo.net>
2016-08-12 15:44:43 +0000

    Symlink the zshrc file after setting it up
    Gets overridden otherwise

--- setup.sh
+++ setup.sh
@@ -8,8 +8,9 @@ git config --global user.email "darrell@mozingo.net"
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -sv ~/.dotfiles/.vimrc ~
-ln -sv ~/.dotfiles/.zshrc ~
-ln -sv ~/.dotfiles/.tmux.conf ~
 vim +PluginInstall
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ln -sv ~/.dotfiles/.zshrc ~
+
+ln -sv ~/.dotfiles/.tmux.conf ~
