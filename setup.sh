009562c22a89c85e0575cebdc66a93e8b2a8d419
Darrell Mozingo <darrell@mozingo.net>
2016-10-03 13:59:29 +0000

    Remove correct zshrc!

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update \
   && sudo apt-get install -y curl vim tmux zsh
-  
+
 git config --global user.name "Darrell Mozingo"
 git config --global user.email "darrell@mozingo.net"
 
@@ -11,7 +11,7 @@ ln -sv ~/.dotfiles/.vimrc ~
 vim +PluginInstall
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-rm -f ~/.dotfiles/.zshrc
+rm -f ~/.zshrc
 ln -sv ~/.dotfiles/.zshrc ~
 
 ln -sv ~/.dotfiles/.tmux.conf ~
