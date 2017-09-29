f729ed78e4b628680197fd7a8848d39c38f26658
Darrell Mozingo <darrell@mozingo.net>
2017-09-29 19:51:44 +0100

    Add p4merge as merge tool

--- setup.sh
+++ setup.sh
@@ -6,6 +6,11 @@ sudo apt-get update \
 git config --global user.name "Darrell Mozingo"
 git config --global user.email "darrell@mozingo.net"
 
+git config --global mergetool.keepBackup false
+git config --global merge.tool p4merge
+git config --global mergetool.p4merge.cmd 'p4merge "$BASE" "$LOCAL" "$REMOTE" "$MERGED"'
+git config --global mergetool.p4mergetool.trustExitCode false
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -sv ~/.dotfiles/.vimrc ~
 vim +PluginInstall
@@ -16,3 +21,5 @@ ln -sv ~/.dotfiles/*.zshrc ~
 git update-index --assume-unchanged ~/.dotfiles/.work-commands.zshrc # don't track future changes
 
 ln -sv ~/.dotfiles/.tmux.conf ~
+
+echo "Get p4merge from http://www.perforce.com/product/components/perforce-visual-merge-and-diff-tools"
