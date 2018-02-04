0cc9903fa7758a086d44619e92e7f7ec3046751a
Darrell Mozingo <darrell@mozingo.net>
2018-02-04 01:34:05 +0000

    Symlink files individually
    Globbing doesn't work here, or I'm doing it wrong.

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ sudo apt-get update \
 git config --global user.name "Darrell Mozingo"
 git config --global user.email "darrell@mozingo.net"
 
-git config --global mergetool.keepBackup false
 git config --global merge.tool p4merge
 git config --global mergetool.p4merge.cmd 'p4merge "$BASE" "$LOCAL" "$REMOTE" "$MERGED"'
 git config --global mergetool.p4mergetool.trustExitCode false
+git config --global mergetool.keepBackup false
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -sv ~/.dotfiles/.vimrc ~
@@ -17,7 +17,8 @@ vim +PluginInstall
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 rm -f ~/.zshrc
-ln -sv ~/.dotfiles/*.zshrc ~
+ln -sv ~/.dotfiles/.zshrc ~
+ln -sv ~/.dotfiles/.work-commands.zshrc ~
 git update-index --assume-unchanged ~/.dotfiles/.work-commands.zshrc # don't track future changes
 
 ln -sv ~/.dotfiles/.tmux.conf ~
