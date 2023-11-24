31f7a8f2ebc5be68e5d9b95a8df7b44e9e2e1090
Darrell Mozingo <darrell@mozingo.net>
2023-11-24 00:02:40 -0500

    Update mac setup script
    Brew install update, files, gitconfig, etc

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
-sudo xcodebuild -license
+# Install Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/darrell/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew install wget git htop tmux zsh vim
+# Common tools
+brew install wget git htop tmux zsh vim editorconfig
 brew install --cask p4v
 
+# Git config
 git config --global user.name "Darrell Mozingo"
 git config --global user.email "darrell@mozingo.net"
+git config --global init.defaultBranch main
 
 git config --global merge.tool p4merge
 git config --global mergetool.p4merge.path /Applications/p4merge.app/Contents/MacOS/p4merge
 git config --global mergetool.p4merge.trustExitCode false
 git config --global mergetool.keepBackup false
 
+# Vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -Fsv ~/.dotfiles/.vimrc ~
 vim +PluginInstall
 
+# Zsh setup
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 rm -f ~/.zshrc
 ln -sv ~/.dotfiles/.zshrc ~
 ln -sv ~/.dotfiles/.work-commands.zshrc ~
 git update-index --assume-unchanged ~/.dotfiles/.work-commands.zshrc # don't track future changes
 
+# tmux setup
 ln -sv ~/.dotfiles/.tmux.conf ~
+
